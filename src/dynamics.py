4954a5a48e514ad74bddb5581dcd4709c32a053f
Siddhant Deshmukh <sd486@exeter.ac.uk>
2021-08-09 09:39:21 +0200

    untested implementation of solvers

--- src/dynamics.py
+++ src/dynamics.py
@@ -1,6 +1,7 @@
 from typing import Union, Dict, List
 import numpy as np
 from src.network import Network
+from scipy.integrate import ode
 
 
 class NetworkDynamics():
@@ -28,12 +29,18 @@ class NetworkDynamics():
 
     return number_densities
 
-  def create_rates_vector(self, number_densities: np.ndarray) -> np.ndarray:
+  def create_rates_vector(self, number_densities: np.ndarray,
+                          temperature=None) -> np.ndarray:
     # Create the vector v(x) = K Exp(Z.T Ln(x)) that includes the stoichiometry
     # into reaction rates
     # 'number_densities' must have the same indexing as species!
     Z = self.network.complex_composition_matrix
-    K = self.network.complex_kinetics_matrix
+
+    if temperature:
+      K = self.network.create_complex_kinetics_matrix(temperature)
+    else:
+      K = self.network.complex_kinetics_matrix
+
     rates_vector = K.dot(np.exp(Z.T.dot(np.log(number_densities))))
 
     return rates_vector
@@ -45,4 +52,78 @@ class NetworkDynamics():
 
     return dynamics_vector
 
-  # Nonlinear ODE solver?
+  def solve(self, timescale: float, initial_number_densities: np.ndarray,
+            initial_time=0, create_jacobian=False, jacobian=None) -> np.ndarray:
+    # TODO:
+    # Add atol, rtol and other solver params
+    def f(t: float, y: np.ndarray, temperature=None) -> List[np.ndarray]:
+      # Create RHS ZDK Exp(Z.T Ln(x))
+      Z = self.network.complex_composition_matrix
+      D = self.network.complex_incidence_matrix
+      rates_vector = self.create_rates_vector(y, temperature)
+
+      return Z @ D @ rates_vector
+
+    if create_jacobian:
+      # Create the Jacobian matrix analytically
+      pass
+    if jacobian:
+      solver = ode(f, jacobian).set_integrator("vode", method='bdf')
+    else:
+      solver = ode(f).set_integrator("vode", method='bdf')
+
+    # Initial values
+    solver.set_initial_value(initial_number_densities, initial_time)
+
+    # TODO: Add better time-stepping control
+    dt = timescale - initial_time
+
+    # TODO: Check for failure and return codes
+    number_densities = []
+    while solver.successful() and solver.t < timescale:
+      number_densities.append(solver.integrate(solver.t + dt))
+
+    return number_densities
+
+  def solve_steady_state(self, initial_number_densities: np.ndarray,
+                         create_jacobian=False, jacobian=None,
+                         tolerance=1e-7) -> Union[np.ndarray, float]:
+    # Solves the CRN dynamics for a steady-state given initial conditions and
+    # returns the steady-state number densities alongside the time to reach
+    # steady-state
+    # Solves the system until the change is less than 'tol'
+    # TODO:
+    # Wrap self.solve() using 'atol', 'rtol'?
+    def f(t: float, y: np.ndarray, temperature=None) -> List[np.ndarray]:
+      # Create RHS ZDK Exp(Z.T Ln(x))
+      Z = self.network.complex_composition_matrix
+      D = self.network.complex_incidence_matrix
+      rates_vector = self.create_rates_vector(y, temperature)
+
+      return Z @ D @ rates_vector
+
+    if create_jacobian:
+      # Create the Jacobian matrix analytically
+      pass
+    if jacobian:
+      solver = ode(f, jacobian).set_integrator("vode", method='bdf')
+    else:
+      solver = ode(f).set_integrator("vode", method='bdf')
+
+    # Initial values
+    solver.set_initial_value(initial_number_densities)
+
+    # TODO: Add better time-stepping control
+    dt = 0.1
+
+    # TODO: Check for failure and return codes
+    y_previous = initial_number_densities.copy()
+    done = False
+
+    while not done:
+      y = solver.integrate(solver.t + dt)
+      if np.abs(y - y_previous).all() < tolerance:
+        done = True
+      y_previous = y.copy()
+
+    return y, solver.t
