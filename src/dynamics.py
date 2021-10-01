fixed functional Jacobian and indexing, still strange discontinuity at low T

src/dynamics.py
@@ -5,6 +5,9 @@ from src.network import Network
 from scipy.integrate import ode
 import sympy
 from numba import jit
+import re
+from itertools import product
+from math import exp  # used in 'eval'
 
 
 # TODO:
@@ -20,6 +23,11 @@ class NetworkDynamics():
     self.symbols = [f"n_{s}" for s in self.species]
     if temperature:
       self.network.temperature = temperature
+
+    # Properties
+    self._temperature = temperature
+    # TODO:
+    # Number densities as properties
     self.initial_number_densities =\
         self.setup_initial_number_densities(initial_number_densities)
     self.number_densities = self.initial_number_densities.copy()
@@ -30,10 +38,7 @@ class NetworkDynamics():
 
     # Rates and Jacobian attributes
     self.rate_dict = self.create_rate_dict()
-    self.jacobian_str = self.create_jacobian()
-
-    # Properties
-    self._temperature = temperature
+    self.jacobian_func = self.create_jacobian()
 
   # TODO:
   # Have number densities and initial number densities as properties?
@@ -74,6 +79,18 @@ class NetworkDynamics():
       product_symbols = [
           f"n_{key}" for key in reaction.stoichiometry[1].keys()]
       for symbol in reactant_symbols:
+        # TODO:
+        # If symbol appears in both reactant and product sides, ignore it
+        # But also check it appears the same number of times, e.g.
+        # H2 + H2 -> H2 + H + H
+        # has 'H2' on both sides, but one side has one more so it's still
+        # a destruction
+        # Alternatively, just leave it and check for zero rates at the end
+        # Idea:
+        # Check if the opposite symbol exists in the dictionary-key list, and
+        # if so, remove it and don't add the new one since it means we're adding
+        # e.g. dict['A'] = 'B' ' - B'
+        # But again, might just be faster to catch them all at the end?
         if symbol in rate_dict.keys():
           rate_dict[symbol].append(f"-{expression}")
         else:
@@ -88,37 +105,60 @@ class NetworkDynamics():
     return rate_dict
 
   def create_jacobian(self) -> np.ndarray:
-    # Create the analytical Jacobian matrix as a 2D array of strings
-    # Uses sympy for the differential (which uses eval)
+    # Create a Jacobian where each entry is a Callable, taking in number densities
+    # as indexed in dynamics.network.species and a temperature
+    # TODO:
+    # Fix indexing for rate dict vs symbols/species
     num_species = len(self.species)
+    # Fill with function that returns zero instead?
     jacobian = np.zeros((num_species, num_species), dtype=object)
-    # Set up d[A]/dt = a_1 + a_2 + ... for each differential expression with A
-    for i, rate in enumerate(self.rate_dict.values()):
-      expression = " + ".join(rate)
-      for j, symbol in enumerate(self.symbols):
-        differential = sympy.diff(expression, symbol)
-        jacobian[i, j] = str(differential)
+    for i, s1 in enumerate(self.symbols):
+      # for i, (s, rate) in enumerate(self.rate_dict.items()):
+      expression = '+'.join(self.rate_dict[s1])
+      # expression = "+".join(rate)
+      for j, s2 in enumerate(self.symbols):
+        differential = sympy.diff(expression, s2)
+        rate = str(differential).replace("Tgas", "T")
+        pattern = r"_[A-Z0-9]*"
+        # TODO:
+        # Do the whole replacement with regex!
+        rate = re.sub(pattern,
+                      lambda s: f"[{self.species.index(s.group()[1:])}]",
+                      rate)
+        jacobian[i, j] = eval(f"lambda T, n: {rate}")
+
+    #   num_species = len(self.species)
+    # jacobian = np.zeros((num_species, num_species), dtype=object)
+    # # Set up d[A]/dt = a_1 + a_2 + ... for each differential expression with A
+    # for i, rate in enumerate(self.rate_dict.values()):
+    #   expression = " + ".join(rate)
+    #   for j, symbol in enumerate(self.symbols):
+    #     differential = sympy.diff(expression, symbol)
+    #     jacobian[i, j] = str(differential)
 
     return jacobian
 
   # @jit(nopython=True)
   def evaluate_jacobian(self, temperature: float,
                         number_densities: np.ndarray) -> np.ndarray:
-    # Evaluate the 'jacobian_str' using sympy
-    # Note that 'number_densities' must have the same indexing as
-    # network species
-    # TODO:
-    # Use some fancy code-gen to write the jacobian into C-like code since
-    # upon evaluation, we just need to pass in temperature and number densities
-    n_dict = {s: n for s, n in zip(self.symbols, number_densities)}
-    n_dict['Tgas'] = temperature
-    rows, cols = self.jacobian_str.shape
-    jacobian = np.zeros((rows, cols))
-    for i in range(rows):
-      for j in range(cols):
-        # Evaluate with temperature and number densities!
-        jacobian[i, j] = sympy.parse_expr(self.jacobian_str[i, j],
-                                          evaluate=True, local_dict=n_dict)
+    # # Evaluate the 'jacobian_str' using sympy
+    # # Note that 'number_densities' must have the same indexing as
+    # # network species
+    # n_dict = {s: n for s, n in zip(self.symbols, number_densities)}
+    # n_dict['Tgas'] = temperature
+    # rows, cols = self.jacobian_str.shape
+    # jacobian = np.zeros((rows, cols))
+    # for i in range(rows):
+    #   for j in range(cols):
+    #     # Evaluate with temperature and number densities!
+    #     jacobian[i, j] = sympy.parse_expr(self.jacobian_str[i, j],
+    #                                       evaluate=True, local_dict=n_dict)
+    # Evaluate the function Jacobian by calling each element with the provided
+    # temperature and number densities
+    jacobian = np.zeros_like(self.jacobian_func, dtype=float)
+    x, y = jacobian.shape
+    for i, j in product(range(x), range(y)):
+      jacobian[i, j] = self.jacobian_func[i, j](temperature, number_densities)
 
     return jacobian
 

tests/limits.py
@@ -1,5 +1,6 @@
 # %%
 # Test temperature limits and fading functions
+from itertools import product
 from sadtools.utilities.chemistryUtilities import gas_density_to_hydrogen_number_density, log_abundance_to_number_density
 from scipy.special import expit
 from typing import Callable, Dict, List
@@ -121,6 +122,7 @@ def setup_number_densities(number_densities_dict, hydrogen_density):
 # # CO network rates
 res_dir = '../res'
 network_file = f'{res_dir}/solar_co_w05.ntw'
+# network_file = f'{res_dir}/simplified_co.ntw'
 # network_file = f'{res_dir}/catalyst_co.ntw'
 # network_file = f'{res_dir}/mp_cno.ntw'
 
@@ -129,11 +131,11 @@ initial_number_densities = {
     "H2": 1e-4,
     # "H2": 1e-12,
     "OH": 1e-12,
-    # "C": 10**(8.39),  # solar
-    # "O": 10**(8.66),  # solar
+    "C": 10**(8.39),  # solar
+    "O": 10**(8.66),  # solar
     "N": 10**(7.83),
-    "C": 10**(8.66),  # C-rich
-    "O": 10**(8.39),  # C-rich
+    # "C": 10**(8.66),  # C-rich
+    # "O": 10**(8.39),  # C-rich
     "CH": 1e-12,
     "CO": 1e-12,
     "CN": 1e-12,
@@ -214,11 +216,16 @@ def setup_dynamics(network: Network, temperature: float):
 def solve_dynamics(dynamics: NetworkDynamics, times: List,
                    limit_rates=False) -> np.ndarray:
   final_number_densities = []
+  jac = dynamics.evaluate_jacobian(dynamics.temperature,
+                                   dynamics.number_densities)
+  print(jac[dynamics.species.index('M')])
+  print(jac[dynamics.species.index('H')])
+  input()
   # TODO:
   # Refactor 'dynamics' to use dynamics.number_densities instead of passing in
   for i, time in enumerate(times):
     final = dynamics.solve(time, dynamics.number_densities,
-                           create_jacobian=False,
+                           create_jacobian=True,
                            limit_rates=limit_rates)[0]
     final_number_densities.append(final)
     print(f"Done {i+1} time of {len(times)}")
@@ -271,16 +278,35 @@ def run_and_plot(temperatures: List, times: List, network: Network,
   plt.savefig(filename, bbox_inches="tight")
 
 
+def enumerated_product(*args):
+  # TODO:
+  # Move to utilities and reference
+  # https://stackoverflow.com/questions/56430745/enumerating-a-tuple-of-indices-with-itertools-product
+  yield from zip(product(*(range(len(x)) for x in args)), product(*args))
+
 # network.to_krome_format('./test.ntw')
 # network.to_cobold_format('./test.dat')
 # test_network = Network.from_krome_file('./test.ntw')
 # print([f"{reaction}\n" for reaction in test_network.reactions])
 
+
 # Kinetics with limits
 # network = Network.from_krome_file("../res/simplified_co.ntw")
-# temperatures = [300, 1000, 3000, 5000, 7500, 10000, 15000, 20000, 30000]
-temperatures = [5000, 10000, 20000]
+temperatures = [300, 1000, 3000, 5000, 7500, 10000, 15000, 20000, 30000]
+# temperatures = [5000, 10000, 20000]
 times = np.logspace(-6, 3, num=10)
+
+# # Test Jacobian
+# for (i, j), (temperature, times) in enumerated_product(temperatures, times):
+#   dynamics = setup_dynamics(network, temperature)
+#   print(dynamics.number_densities)
+#   for key, val in dynamics.rate_dict.items():
+#     print(f"{key}: {val}\n")
+#   print(dynamics.evaluate_jacobian(temperature, dynamics.number_densities))
+#   print(dynamics.species)
+#   print(dynamics.symbols)
+#   input()
+
 colours = ['b', 'g', 'r', 'gold', 'purple', 'violet', 'sienna', 'teal']
 print(f"Solving unlimited rates case.")
 filename = f"../out/figs/solar_network_unlimited_simplified.png"

tests/parser.py
@@ -1,4 +1,5 @@
 # Test sympy parsing vs function-Jacobian creation
+from typing import Dict
 from numba import jit
 from itertools import product
 from src.network import Network
@@ -41,28 +42,53 @@ def evaluate_functional_jacobian(func_jac: np.ndarray,
   return jac
 
 
+def symbolic_RHS(dynamics: NetworkDynamics, temperature: float):
+  # Symbolically create the RHS vector of rates ZDK Exp(Z.T Ln(x))
+  # WARNING: Does not sanitise input to eval!
+  dynamics.temperature = temperature
+  Z = dynamics.network.complex_composition_matrix
+  D = dynamics.network.complex_incidence_matrix
+  K = dynamics.network.complex_kinetics_matrix
+
+  # TODO:
+  # Do this symbolically with Sympy. It should only be called ONCE when
+  # creating the Jacobian function
+  S = Z @ D  # no symbols
+  # symbols are in 'number_densities'?
+  rates_vector = K.dot(np.exp(Z.T.dot(np.log(dynamics.number_densities))))
+
+  # symbolic vector
+  rhs = S.dot(rates_vector)
+
+
 initial_number_densities = {
     "H": 1e12,
     "H2": 1e-4,
+    # "H2": 1e-12,
     "OH": 1e-12,
-    "C": 10**(8.39),  # solar
-    "O": 10**(8.66),  # solar
-    # "C": 10**(8.66),  # C-rich
-    # "O": 10**(8.39),  # C-rich
+    # "C": 10**(8.39),  # solar
+    # "O": 10**(8.66),  # solar
+    "N": 10**(7.83),
+    "C": 10**(8.66),  # C-rich
+    "O": 10**(8.39),  # C-rich
     "CH": 1e-12,
     "CO": 1e-12,
+    "CN": 1e-12,
+    "NH": 1e-12,
+    "NO": 1e-12,
+    "C2": 1e-12,
+    "O2": 1e-12,
+    "N2": 1e-12,
     "M": 1e11,
 }
-
 # network = Network.from_krome_file('../res/catalyst_co.ntw')
-network = Network.from_krome_file('../res/simplified_co.ntw')
+network = Network.from_krome_file('../res/solar_co_w05.ntw')
 dynamics = NetworkDynamics(network, initial_number_densities, temperature=5700)
-jac_func = create_functional_jacobian(dynamics)
-# for temperature in [300, 3000, 5000, 10000, 15000, 20000, 25000, 30000]:
-#   dynamics.evaluate_jacobian(temperature, dynamics.number_densities)
-#   eval_jac = evaluate_functional_jacobian(
-#       jac_func, temperature, dynamics.number_densities)
+print(dynamics.number_densities)
+exit()
 
-print(network.complex_composition_matrix)
-print(network.species)
-print(network.complexes)
+for temperature in [300, 3000, 5000, 10000, 15000, 20000, 25000, 30000]:
+  dynamics.evaluate_jacobian(temperature, dynamics.number_densities)
+  for i, time in enumerate(np.logspace(-6, 3, num=50)):
+    dynamics.solve(time, dynamics.number_densities, create_jacobian=True)
+    print(f"Done time {i + 1} of 50.")