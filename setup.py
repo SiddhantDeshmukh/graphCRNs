411296cfedef7112ca21aea93fbc18c0e3298c80
Siddhant Deshmukh <sd486@exeter.ac.uk>
2021-10-15 10:32:31 +0200

    trying to install

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as fh:
   long_description = fh.read()
 
 setuptools.setup(
-    name="graphCRN",
+    name="graphcrn",
     version="0.0.1",
     author="Siddhant A. Deshmukh",
     author_email="siddhant593@gmail.com",
     description="A set of tools to investigate chemical reaction networks as graphs.",
     long_description=long_description,
     url="https://github.com/SiddhantDeshmukh/graphCRNs",
-    packages=setuptools.find_packages('src'),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
